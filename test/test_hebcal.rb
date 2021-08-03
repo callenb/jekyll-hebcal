a9175c5d9729f0752f5cc9b78e0f0a0fd57bff64
Barrie Callender <barrie@callenb.org>
2021-08-03 21:02:54 +0100

    made cleaner

--- test/test_hebcal.rb
+++ test/test_hebcal.rb
@@ -8,9 +8,11 @@ class TestMyBirthdate < MiniTest::Test #:nodoc:
     end
     
     def test_how_to_test
-        tag = Jekyll::Hebcal::HebcalTag.parse('hebcal', "10 June 1963", Tokenizer.new(""), ParseContext.new)
+        markup = '10 Jun 1963'
+        expected = '10 June 1963 (18 Sivan 5723)'
+        tag = Jekyll::Hebcal::HebcalTag.parse(@tagname, markup, Tokenizer.new(""), ParseContext.new)
         assert_equal('jekyll::hebcal::hebcaltag', tag.name)
-        assert_equal('10 June 1963 (18 Sivan 5723)', tag.render(Context.new))
+        assert_equal(expected, tag.render(Context.new))
     end
     
   end
